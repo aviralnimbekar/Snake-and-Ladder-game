739e50c496bc2751684e66a7d5c80d0376030138
amitnimbekar <amit.nimbekar@actifio.com>
2021-08-12 12:31:15 +0530

    ADDING : Player 1 and initialising board

--- src/main/java/com/Bridgelabs/SnakeLadder.java
+++ src/main/java/com/Bridgelabs/SnakeLadder.java
@@ -3,13 +3,16 @@ package com.Bridgelabs;
 /**
  * Objective - Simulates Snake & Ladder game
  *
- * @author  - Aviral N
- * @version - 1.0
+ * @author - Aviral N
+ * @version - 1.1
  * date     - 12-08-2021
  */
 public class SnakeLadder {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game");
+
+        //variable player1 defines position for PLAYER-1, initially at zero position
+        int player1 = 0;
     }
 }
