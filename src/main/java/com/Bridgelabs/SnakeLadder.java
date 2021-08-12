35784201d9e0a073519a37c5f1a75bdaeabdb764
amitnimbekar <amit.nimbekar@actifio.com>
2021-08-12 12:02:53 +0530

    Welcome message

---
+++ src/main/java/com/Bridgelabs/SnakeLadder.java
@@ -0,0 +1,15 @@
+package com.Bridgelabs;
+
+/**
+ * Objective - Simulates Snake & Ladder game
+ *
+ * @author  - Aviral N
+ * @version - 1.0
+ * date     - 12-08-2021
+ */
+public class SnakeLadder {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder game");
+    }
+}
