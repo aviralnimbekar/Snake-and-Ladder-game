d058e5df0906b0ad160e028221f161f71b5d1c7d
amitnimbekar <amit.nimbekar@actifio.com>
2021-08-12 16:45:44 +0530

    ADDING : Counter for total round played

--- src/main/java/com/Bridgelabs/SnakeLadder.java
+++ src/main/java/com/Bridgelabs/SnakeLadder.java
@@ -4,7 +4,7 @@ package com.Bridgelabs;
  * Objective - Simulates Snake & Ladder game
  *
  * @author - Aviral N
- * @version - 1.5
+ * @version - 1.6
  * date     - 12-08-2021
  */
 public class SnakeLadder {
@@ -16,9 +16,14 @@ public class SnakeLadder {
         int player1 = 0;
         UtilityClass dice = new UtilityClass();            //Object of utility class
 
+        //initiating counter to count total number of round played
+        int totalRound = 0;
+
         //using a while loop to repeat entire steps till winning point
         while (player1 != 100) {
 
+            totalRound++;
+            System.out.println("Round----- " + totalRound);
             //Rolling dice
             player1 += dice.rollDice();                        //Calling diceRoll method
             System.out.println("Intermediate Position " + player1);
@@ -27,5 +32,6 @@ public class SnakeLadder {
             player1 = dice.option(player1);                    //Calling option method for Snake Ladder or NoPlay
             System.out.println("final Position " + player1);
         }
+        System.out.println("Total Round played " + totalRound);
     }
 }
