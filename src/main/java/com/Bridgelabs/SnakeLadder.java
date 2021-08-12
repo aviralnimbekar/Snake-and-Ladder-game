7bb7582220bccbc77b5b328431c5a04130f1f55f
amitnimbekar <amit.nimbekar@actifio.com>
2021-08-12 15:47:24 +0530

    ADDING : Condition for Snake Ladder or Nopay

--- src/main/java/com/Bridgelabs/SnakeLadder.java
+++ src/main/java/com/Bridgelabs/SnakeLadder.java
@@ -4,7 +4,7 @@ package com.Bridgelabs;
  * Objective - Simulates Snake & Ladder game
  *
  * @author - Aviral N
- * @version - 1.2
+ * @version - 1.3
  * date     - 12-08-2021
  */
 public class SnakeLadder {
@@ -14,9 +14,14 @@ public class SnakeLadder {
 
         //variable player1 defines position for PLAYER-1, initially at zero position
         int player1 = 0;
+        UtilityClass dice = new UtilityClass();            //Object of utility class
 
-        UtilityClass dice = new UtilityClass();
-        player1 += dice.rollDice();            //Calling diceRoll method
+        //Rolling dice
+        player1 += dice.rollDice();                        //Calling diceRoll method
         System.out.println("Position " + player1);
+
+        //Checking for condition - Snake Ladder or Noplay
+        player1 = dice.option(player1);                    //Calling option method for Snake Ladder or NoPlay
+        System.out.println("final Position " + player1);
     }
 }
--- src/main/java/com/Bridgelabs/UtilityClass.java
+++ src/main/java/com/Bridgelabs/UtilityClass.java
@@ -2,6 +2,8 @@ package com.Bridgelabs;
 
 public class UtilityClass {
 
+    static int dice;
+
     /**
      * Rolling dice to get number between number 1 to 6
      *
@@ -9,8 +11,31 @@ public class UtilityClass {
      */
     public int rollDice() {
 
-        int dice = (int) ((Math.random() * 6) + 1);
+        dice = (int) ((Math.random() * 6) + 1);
         System.out.println("Dice " + dice);
         return dice;
+    }
+
+    /**
+     * checking condition for snake, ladder or no play
+     *
+     * @param player Player1
+     * @return player (final position of player for particular round)
+     */
+    public int option(int player) {
+
+        int LADDER = 1, SNAKE = 2;      // NOPLAY = 0
+        int condition = (int) (Math.random() * 3);
+
+        if (condition == LADDER) {
+            System.out.println("<LADDER>");
+            player += dice;                    //adding dice value for one more time in to current position
+        } else if (condition == SNAKE) {
+            System.out.println("<SNAKE>");
+            player -= dice;                    //subtracting dice value from current position
+        } else {
+            System.out.println("<NOPLAY>");    //in case of no play player remains in current position
+        }
+        return player;
     }
 }
