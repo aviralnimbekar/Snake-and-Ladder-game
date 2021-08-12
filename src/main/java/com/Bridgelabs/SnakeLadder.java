100c590d622de7f86210b26d6f094bdabaca03a3
amitnimbekar <amit.nimbekar@actifio.com>
2021-08-12 15:59:24 +0530

    ADDING : Loop to reach upto winning point

--- src/main/java/com/Bridgelabs/SnakeLadder.java
+++ src/main/java/com/Bridgelabs/SnakeLadder.java
@@ -4,7 +4,7 @@ package com.Bridgelabs;
  * Objective - Simulates Snake & Ladder game
  *
  * @author - Aviral N
- * @version - 1.3
+ * @version - 1.4
  * date     - 12-08-2021
  */
 public class SnakeLadder {
@@ -16,12 +16,16 @@ public class SnakeLadder {
         int player1 = 0;
         UtilityClass dice = new UtilityClass();            //Object of utility class
 
-        //Rolling dice
-        player1 += dice.rollDice();                        //Calling diceRoll method
-        System.out.println("Position " + player1);
+        //using a while loop to repeat entire steps till winning point
+        while (player1 <= 100) {
 
-        //Checking for condition - Snake Ladder or Noplay
-        player1 = dice.option(player1);                    //Calling option method for Snake Ladder or NoPlay
-        System.out.println("final Position " + player1);
+            //Rolling dice
+            player1 += dice.rollDice();                        //Calling diceRoll method
+            System.out.println("Position " + player1);
+
+            //Checking for condition - Snake Ladder or Noplay
+            player1 = dice.option(player1);                    //Calling option method for Snake Ladder or NoPlay
+            System.out.println("final Position " + player1);
+        }
     }
 }
